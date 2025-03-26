8401717b204f1f8bceafc17eec6833876cee5d67
winston <andyredjohn@gmail.com>
2025-03-26 11:23:15 -0500

    Implemented admin authentication with Devise and ActiveAdmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+if User.find_by(email: 'admin@example.com').nil?
+    User.create!(
+      email: 'admin@example.com',
+      password: 'password123',
+      password_confirmation: 'password123',
+      first_name: 'Admin',
+      last_name: 'User',
+      admin: true
+    )
+    puts 'Admin user created'
+  end
