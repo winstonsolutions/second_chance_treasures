57ce504b955421dd45dbd0d186abc17a9ce2575e
winston <andyredjohn@gmail.com>
2025-04-04 12:08:36 -0500

    can edit location after login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,38 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
+  # Override the update method to ensure address fields are properly saved
+  def update
+    # If password was not provided, remove it from params to allow update without password
+    if params[:user][:password].blank?
+      # If password is blank, allow updating without current password
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+      params[:user].delete(:current_password)
+
+      # Update without requiring the current password
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+      prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+      # Direct database update skipping password validation
+      updated = resource.update(account_update_params)
+
+      # Handle update result
+      if updated
+        set_flash_message_for_update(resource, prev_unconfirmed_email)
+        bypass_sign_in resource, scope: resource_name
+        respond_with resource, location: after_update_path_for(resource)
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
+      end
+    else
+      # If password is provided, follow the normal Devise update process
+      super
+    end
+  end
+
   protected
 
   def configure_sign_up_params
@@ -28,5 +60,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
       :postal_code,
       :province_id
     ])
+  end
+
+  # Override the path after updating
+  def after_update_path_for(resource)
+    my_account_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,12 @@ class User < ApplicationRecord
   belongs_to :province, optional: true
   has_many :orders, dependent: :destroy
 
-  # Make address_line1, city, postal_code, and province_id optional during registration
-  validates :address_line1, :city, :postal_code, :province_id, presence: true, on: :update
+  # Validations for address fields
+  # These are not required during sign up but should be present when updating address info
+  validates :address_line1, :city, :postal_code, :province_id,
+            presence: true,
+            on: :update,
+            unless: -> { address_line1.blank? && city.blank? && postal_code.blank? && province_id.blank? }
 
   def self.ransackable_attributes(auth_object = nil)
     ["email", "first_name", "last_name", "created_at", "id", "address_line1", "city", "postal_code", "province_id"]
